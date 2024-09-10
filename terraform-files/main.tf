b6e50a0aa31e0b7324b16574a56ece79520787e8
Neeraja037 <142163392+Neeraja037@users.noreply.github.com>
2024-09-10 14:36:07 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "prod-server" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
   key_name = "keypair2"
-  vpc_security_group_ids = ["sg-0d78a2ac80c38ca21"]
+  vpc_security_group_ids = ["sg-094bd724ab15ed9b9"]
   connection {
      type = "ssh"
      user = "ubuntu"
