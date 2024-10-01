305a1b4a095bc65366e26707e08f7474c92dfd4e
Neeraja037 <142163392+Neeraja037@users.noreply.github.com>
2024-10-01 15:37:21 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,14 +1,19 @@
-resource "aws_instance" "prod-server" {
+resource "aws_instance" "test-server" {
   ami = "ami-05134c8ef96964280"
   instance_type = "t2.micro"
   key_name = "staragile"
   vpc_security_group_ids = ["sg-0fe8a7968d549f29f"]
+  provisioner "remote-exec" {
   connection {
-     type = "ssh"
-     user = "ec2-user"
-     private_key = file("./staragile.pem")
-     host = self.public_ip
-     }
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = self.public_ip
+    private_key = file("~/.ssh/your_key.pem")
+  }
+
+  script = "path/to/your/script.sh"
+}
+
   provisioner "remote-exec" {
      inline = ["echo 'wait to start the instance' "]
   }
